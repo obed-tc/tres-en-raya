4c44abcf7cabb6f5e30c98bed1ef90c5a6332e36
obed-tc <castroobed53@gmail.com>
2024-09-29 11:39:58 -0400

    feat: Add footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full text-center">
+      Desarrollado por <span className="text-blue-400">obed-tc</span>
+    </footer>
+  );
+}
+
+export default Footer;
