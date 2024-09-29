004fdfd5239c6192b0449c1e823907eb40b1d44f
obed-tc <castroobed53@gmail.com>
2024-09-29 11:42:06 -0400

    feat: Add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import { useState } from "react";
 import Modal from "./Components/Modal";
 import { TURNS } from "./Constants/consts";
 import { winnersCombined } from "./Constants/consts";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 const Square = ({ children, updateBoard, index }) => {
   const clickSquare = () => {
     updateBoard(index);
   };
   return (
     <div
-      className="w-20 h-20 rounded-xl border border-gray-300 flex items-center justify-center  font-semibold text-4xl"
+      className={`w-24 h-24  flex items-center justify-center  font-semibold text-4xl border-white  ${
+        ![6, 7, 8].includes(index) ? "border-b-4" : " "
+      }
+      ${![0, 3, 6, 9].includes(index) ? "border-l-4" : " "}
+      `}
       onClick={clickSquare}
     >
       {children}
@@ -55,37 +61,46 @@ const App = () => {
   };
 
   return (
-    <main className="min-h-screen bg-gradient-to-r from-black via-lime-800 to-black text-white flex flex-col items-center justify-center font-abc">
-      <h1 className="text-4xl absolute top-10 font-bold">TRES EN RAYA</h1>
+    <div className="min-h-screen bg-gradient-to-r from-black via-[#030037] to-black text-white font-abc flex flex-col">
+      <Header />
+      <main className="flex flex-col items-center justify-center flex-1">
+        <label className=" text-3xl">TURNO DE </label>
 
-      <label className=" text-3xl">TURNO DE </label>
-      <div className="text-3xl flex items-center">
-        <div
-          className={
-            "py-3 px-5 rounded-lg font-bold " +
-            (turn === "X" ? " bg-lime-500 text-black" : "")
-          }
-        >
-          X
+        <div className="text-3xl flex items-center">
+          <div
+            className={`py-3 px-5 rounded-lg font-bold " +
+            ${turn === "X" ? "text-design-x text-black" : ""}
+          `}
+          >
+            X
+          </div>
+          <div
+            className={
+              "py-3 px-5 rounded-lg font-bold " +
+              (turn === "O" ? " text-design-o text-black" : "")
+            }
+          >
+            O
+          </div>
         </div>
-        <div
-          className={
-            "py-3 px-5 rounded-lg font-bold " +
-            (turn === "O" ? " bg-lime-500 text-black" : "")
-          }
-        >
-          O
-        </div>
-      </div>
-      <section className="grid grid-cols-3 gap-6 mt-5">
-        {board.map((item, index) => {
-          return (
-            <Square key={index} updateBoard={modifiedBoard} index={index}>
-              {item}
-            </Square>
-          );
-        })}
-      </section>
+        <section className="grid grid-cols-3  mt-5">
+          {board.map((item, index) => {
+            return (
+              <Square key={index} updateBoard={modifiedBoard} index={index}>
+                <span
+                  className={`${
+                    item == "O" ? "text-design-o" : "text-design-x"
+                  }  text-[70px] `}
+                >
+                  {item}
+                </span>
+              </Square>
+            );
+          })}
+        </section>
+      </main>
+      <Footer />
+
       <Modal isOpen={winner != null} onClose={() => resetGame()}>
         <div className=" ">
           <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
@@ -107,7 +122,7 @@ const App = () => {
           </div>
         </div>
       </Modal>
-    </main>
+    </div>
   );
 };
 
